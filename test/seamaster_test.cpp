dcbb0cf008f3fb8cda495652b620addda492ca40
Jim Menard <jim.menard@chloeandisabel.com>
2017-09-23 17:39:09 -0400

    print test timing

--- test/seamaster_test.cpp
+++ test/seamaster_test.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/resource.h>
 #include "seamaster_test.h"
 #include "../src/loader.h"
 #include "../src/cursor.h"
@@ -33,6 +34,20 @@ int test_num_errors() {
   return results.num_errors;
 }
 
+void print_time() {
+  rusage usage;
+  getrusage(RUSAGE_SELF, &usage);
+  printf("\n\nFinished in %ld.%06ld seconds\n", usage.ru_utime.tv_sec,
+         (long)usage.ru_utime.tv_usec);
+}
+
+void print_results() {
+  printf("\nTests run: %d, tests passed: %d, tests failed: %d\n",
+         results.num_tests, results.num_tests - results.num_errors,
+         results.num_errors);
+  printf("done\n");
+}
+
 void run_tests() {
   test_formatter();
   test_load();
@@ -44,10 +59,8 @@ void run_tests() {
 
 void run_tests_and_print_results() {
   run_tests();
-  printf("\n\nTests run: %d, tests passed: %d, tests failed: %d\n",
-         results.num_tests, results.num_tests - results.num_errors,
-         results.num_errors);
-  printf("done\n");
+  print_time();
+  print_results();
 }
 
 // ================ main ================
